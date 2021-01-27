32ec628a038cbdf736d789ea52d525cf86c624f4
safwan <safwanali137@gmail.com>
2021-01-27 17:26:38 +0600

    Updates

---
+++ precache-manifest.cd51f49f39fbce75b2ba84a891ca0a2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe472859c3dbf2fd914481963dbce877",
+    "url": "/photogalleryapp/index.html"
+  },
+  {
+    "revision": "f4e90b2f2e7f35a02793",
+    "url": "/photogalleryapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8de900b0583f5b9792c9",
+    "url": "/photogalleryapp/static/css/main.b05361eb.chunk.css"
+  },
+  {
+    "revision": "f4e90b2f2e7f35a02793",
+    "url": "/photogalleryapp/static/js/2.56dc0edc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/photogalleryapp/static/js/2.56dc0edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de900b0583f5b9792c9",
+    "url": "/photogalleryapp/static/js/main.285321f4.chunk.js"
+  },
+  {
+    "revision": "3d998ddc48d8f147df1d",
+    "url": "/photogalleryapp/static/js/runtime-main.41699745.js"
+  }
+]);
